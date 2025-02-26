936b9c7d0c87ef78f5ae9c0be034fdcaf809f4a9
QI-D <qid14@outlook.com>
2025-02-26 15:06:52 -0800

    Add constants
    - Defined `BASE_URL` for API requests
    - Added authentication endpoints for user registration and login
    - Included user-related endpoints for fetching logged-in user info
    - Defined category-related endpoints for CRUD operations
    - Added product-related endpoints for creation, updates, search, and
    retrieval
    - Included address endpoint for saving user addresses
    - Added order-related endpoints for creating, fetching, and filtering
    orders
    
    Signed-off-by: QI-D <qid14@outlook.com>

---
+++ src/utils/constants.jsx
@@ -0,0 +1,32 @@
+export const BASE_URL = 'http://localhost:8080';
+
+// auth endpoints
+export const USER_REGISTER_API = '/auth/register';
+export const USER_LOGIN_API = '/auth/login';
+
+// user endpoints
+export const USER_INFO_API = '/user/my-info';
+
+// category endpoints
+export const CREATE_CATEGORY_API = '/category/create';
+export const UPDATE_CATEGORY_API = '/category/update';
+export const GET_ALL_CATEGORY_API = '/category/get-all';
+export const GET_CATEGORY_BY_ID_API = '/category/get-category-by-id';
+export const DELETE_CATEGORY_API = '/category/delete';
+
+// product endpoints
+export const CREATE_PRODUCT_API = '/product/create';
+export const UPDATE_PRODUCT_API = '/product/update';
+export const GET_ALL_PRODUCT_API = '/product/get-all';
+export const SEARCH_PRODUCT_API = '/product/search';
+export const GET_PRODUCT_BY_CATEGORY_API = '/product/get-by-category-id';
+export const GET_PRODUCT_BY_ID_API = '/product/get-product-by-id';
+export const DELETE_PRODUCT_API = '/product/delete';
+
+// address endpoints
+export const ADDRESS_API = '/address/save';
+
+// order endpoints
+export const CREATE_ORDER_API = '/order/create';
+export const GET_ALL_ORDER_API = '/order/get-all';
+export const FILTER_ORDER_API = '/order/filter';
