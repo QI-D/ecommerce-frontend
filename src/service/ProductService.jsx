66471f3383e1708195eeb9fd8ee201810e2018d5
QI-D <qid14@outlook.com>
2025-02-26 15:08:34 -0800

    add product service
    Signed-off-by: QI-D <qid14@outlook.com>

---
+++ src/service/ProductService.jsx
@@ -0,0 +1,71 @@
+import axios from "axios";
+import {
+    BASE_URL,
+    CREATE_PRODUCT_API,
+    UPDATE_PRODUCT_API,
+    GET_ALL_PRODUCT_API,
+    SEARCH_PRODUCT_API,
+    GET_PRODUCT_BY_CATEGORY_API,
+    GET_PRODUCT_BY_ID_API,
+    DELETE_PRODUCT_API
+} from "../../utils/constants";
+import { getHeaders } from "./ApiService";
+
+const createProduct = async (formData) => {
+    const response = await axios.post(`${BASE_URL}${CREATE_PRODUCT_API}`, formData, {
+        headers: {
+            ...getHeaders(),
+            'Content-Type': 'multipart/form-data'
+        }
+    });
+    return response.data;
+};
+
+const updateProduct = async (productId, formData) => {
+    const response = await axios.put(`${BASE_URL}${UPDATE_PRODUCT_API}/${productId}`, formData, {
+        headers: {
+            ...getHeaders(),
+            'Content-Type': 'multipart/form-data'
+        }
+    });
+    return response.data;
+};
+
+const getAllProducts = async () => {
+    const response = await axios.get(`${BASE_URL}${GET_ALL_PRODUCT_API}`);
+    return response.data;
+};
+
+const searchProducts = async (searchValue) => {
+    const response = await axios.get(`${BASE_URL}${SEARCH_PRODUCT_API}`, {
+        params: { searchValue }
+    });
+    return response.data;
+};
+
+const getProductsByCategory = async (categoryId) => {
+    const response = await axios.get(`${BASE_URL}${GET_PRODUCT_BY_CATEGORY_API}/${categoryId}`);
+    return response.data;
+};
+
+const getProductById = async (productId) => {
+    const response = await axios.get(`${BASE_URL}${GET_PRODUCT_BY_ID_API}/${productId}`);
+    return response.data;
+};
+
+const deleteProduct = async (productId) => {
+    const response = await axios.delete(`${BASE_URL}${DELETE_PRODUCT_API}/${productId}`, {
+        headers: getHeaders()
+    });
+    return response.data;
+};
+
+export { 
+    createProduct,
+    updateProduct,
+    getAllProducts,
+    searchProducts,
+    getProductsByCategory,
+    getProductById,
+    deleteProduct
+};
