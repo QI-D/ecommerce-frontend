7d72ae932abd59cb3ed88161469d201907f1ff2b
QI-D <qid14@outlook.com>
2025-03-03 14:20:37 -0800

    add home page
    Signed-off-by: QI-D <qid14@outlook.com>

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import { ProtectedRoute, AdminRoute } from './service/Guard';
 import Navbar from './component/common/Navbar';
 import Footer from './component/common/Footer';
 import { CartProvider } from './component/context/cartContext';
+import Home from './component/pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <CartProvider>
         <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Home/> } />
+          </Routes>
         <Footer />
-        <Routes>
-          
-        </Routes>
       </CartProvider>
     </BrowserRouter>
   );
